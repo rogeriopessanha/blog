b4658072c255ce2c316d5c43364a24f5daaf0270
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-02-27 17:46:50 -0300

    att

--- index.js
+++ index.js
@@ -22,24 +22,80 @@ app.use(express.static('public'))
 
 
 //body parser
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 //database
 connection.authenticate()
-.then(() => {
-    console.log('conectado com o banco de dados')
-}).catch((error) => {
-    console.log(error)
-})
+    .then(() => {
+        console.log('conectado com o banco de dados')
+    }).catch((error) => {
+        console.log(error)
+    })
 
 app.use('/', rotaCategoria)
 app.use('/', rotaArtigos)
 
-//renderiza na tela
+//renderiza na tela principal
 app.get('/', (req, res) => {
-    Artigo.findAll().then(artigo => {
-        res.render('index', {artigo: artigo})
+    Artigo.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    })
+        .then(artigo_home => {
+
+            Categoria.findAll()
+                .then(categoria_home => {
+                    res.render('app', { artigos: artigo_home, categoria: categoria_home })
+                })
+
+        })
+})
+
+
+//renderiza o slug
+app.get('/:slug', (req, res) => {
+    var slug = req.params.slug
+    Artigo.findOne({
+        where: {
+            slug: slug
+        }
+    })
+        .then(artigo => {
+            if (artigo != undefined) {
+                Categoria.findAll()
+                    .then(categoria => {
+                        res.render('artigo', { artigo: artigo, categoria: categoria })
+                    })
+            } else {
+                res.redirect('/')
+            }
+        })
+        .catch(error => {
+            res.redirect('/')
+        })
+})
+
+app.get('/categoria/:slug', (req, res) => {
+    var slug = req.params.slug
+    Categoria.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Artigo}]
+    })
+    .then(categoria => {
+        if (categoria != undefined) {
+            Categoria.findAll().then(categorias => {
+                res.render('app', {artigos: categoria.artigos, categoria: categorias})
+            })
+        }else{
+            res.redirect('/')
+        }
+    })
+    .catch(error => {
+        res.redirect('/')
     })
 })
 
